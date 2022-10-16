36230c214fe889cdda5bf8d65a483d7a1f536dfa
Carlos Lombardi <carlombardi@gmail.com>
2022-10-16 21:12:34 +0200

    se agrega posibilidad de indicar el nombre del PDF

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 function App() {
     const [imageData, setImageData] = useState();
     const [pdfData, setPdfData] = useState();
+    const [pdfName, setPdfName] = useState("");
+    const [launchLoading, setLaunchLoading] = useState(true);
 
     useEffect(() => {
         const fetchImage = async () => {
@@ -18,18 +20,50 @@ function App() {
 
     useEffect(() => {
         const fetchPdf = async () => {
+            const queryParams = pdfName && pdfName.length > 0 ? `?filename=${pdfName}` : "";
             // cfr. https://stackoverflow.com/questions/60454048/how-does-axios-handle-blob-vs-arraybuffer-as-responsetype
-            const fetchedData = await axios.get("http://localhost:3123/pdf", { responseType: "blob" });
+            const fetchedData = await axios.get(`http://localhost:3123/pdf${queryParams}`, { responseType: "blob" });
             const pdfBlob = new Blob([ fetchedData.data ], { type: "application/pdf" });
             const virtualUrl = URL.createObjectURL(pdfBlob);
             setPdfData(virtualUrl);
         };
-        fetchPdf();
-    }, []);
+        if (launchLoading) {
+            setLaunchLoading(false);
+            setPdfData(null);
+            fetchPdf();
+        }
+    }, [launchLoading, pdfName]);
 
     return (
         <div style={{ display: "flex", flexDirection: "row"}}>
             <div style={{ display: "flex", flexDirection: "column", width: "60%", marginBottom: "2rem"}}>
+                <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
+                    <div style={{ marginRight: "2rem" }}>Nombre del archivo PDF</div>
+                    <div style={{flexGrow: 1}}>
+                        <input style={{width: "100%"}} value={pdfName} onChange={
+                            event => setPdfName(event.target.value)
+                        }/>
+                    </div>
+                    <div style={{ marginLeft: "2rem" }} onClick={() => {
+                        setLaunchLoading(true);
+                    }}>
+                        <button>Cargar</button>
+                    </div>
+                </div>
+                {pdfData && <div style={{ marginTop: "2rem"}}>
+                    Documento PDF desde response de API call
+                </div>}
+                {pdfData && <div style={{ minHeight: "50rem", }}>
+                    { /* https://pspdfkit.com/blog/2018/open-pdf-in-your-web-app/ */ }
+                    <object
+                        data={pdfData}
+                        type="application/pdf"
+                        width="100%"
+                        height="100%"
+                    >
+                        Acá viene el PDF
+                    </object>
+                </div>}
                 <div>
                     Imagen desde URL
                 </div>
@@ -49,20 +83,8 @@ function App() {
                         alt="Imagen obtenida desde el server"
                     />
                 </div>}
-                {pdfData && <div style={{ marginTop: "2rem"}}>
-                    Documento PDF desde response de API call
-                </div>}
-                {pdfData && <div style={{ minHeight: "50rem", }}>
-                    { /* https://pspdfkit.com/blog/2018/open-pdf-in-your-web-app/ */ }
-                    <object
-                        data={pdfData}
-                        type="application/pdf"
-                        width="100%"
-                        height="100%"
-                    />
-                </div>}
             </div>
-            <div>
+            <div style={{ marginLeft: "20px" }}>
                 Hola
             </div>
         </div>
